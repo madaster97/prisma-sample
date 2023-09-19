dcac330a7f6ffc8c918fd05d367486ca1d4dfbfb
Adam Strickland <adstrick@epic.com>
2023-09-19 15:07:39 -0500

    Add prisma user schema

---
+++ server/client.ts
@@ -0,0 +1,6 @@
+// https://www.prisma.io/docs/guides/performance-and-optimization/connection-management#prismaclient-in-long-running-applications
+import { PrismaClient } from '@prisma/client'
+
+let prisma = new PrismaClient()
+
+export default prisma
--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import prisma from '../client'
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, _next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  const allUsers = await prisma.user.findMany();
+  const usersText: string[] = allUsers.map((user) => `${user.name} [${user.id}] at ${user.email}`);
+  res.render('users', { title: 'Viewing Users', usersText });
 });
 
 module.exports = router;
